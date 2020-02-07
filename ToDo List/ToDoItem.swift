e3496a6fef266e84582ccb646c94f80694256348
Ronan <manvelia@bc.edu>
2020-02-07 02:22:51 -0500

    Completed 5.6

---
+++ ToDo List/ToDoItem.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoItem.swift
+//  ToDo List
+//
+//  Created by Ronan Manvelian on 2/7/20.
+//  Copyright © 2020 Ronan Manvelian. All rights reserved.
+//
+
+import Foundation
+
+struct ToDoItem {
+    var name: String
+    var date: Date
+    var notes: String
+}
